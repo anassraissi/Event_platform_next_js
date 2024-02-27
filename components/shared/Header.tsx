158b4002d817d8102d1a21684d1a07b6264a10fe
ANASS RAISSI <88003906+anassraissi@users.noreply.github.com>
2024-02-27 13:13:30 +0100

    initaite header and footer for mobile and web look

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -2,33 +2,40 @@ import { SignIn, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import Link from 'next/link'
 import React from 'react'
 import { Button } from '../ui/button'
-import NavItem  from '../shared/NavItem';
-import NavMobile from './NavMobile';
-
+import NavItem from './NavItem'
+import Image from 'next/image'
+import  MobileNav  from "../shared/NavMobile";
 const Header = () => {
   return (
     <header className="w-full border-b">
-    <div className="wrapper flex items-center justify-between">
+      <div className="wrapper flex items-center justify-between">
         <Link href="/" className="w-36">
-            <img src="/assets/images/logo.svg" width={128} height={38} alt='Evently logo'></img>
+          <Image 
+            src="/assets/images/logo.svg" width={128} height={38}
+            alt="Evently logo" 
+          />
         </Link>
-        <nav className='md:flex-between hidden w-full max-w-xs'>
-        <NavItem/>
-        </nav>
-    <div className="flex w-32 justify-end gap-3">
-      <SignedIn> 
-        <UserButton afterSignOutUrl='/'/>
-        <NavMobile/>
-      </SignedIn>
-    <SignedOut>
+
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItem />
+          </nav>
+        </SignedIn>
+
+        <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+            <MobileNav />
+          </SignedIn>
+          <SignedOut>
             <Button asChild className="rounded-full" size="lg">
               <Link href="/sign-in">
                 Login
               </Link>
             </Button>
-     </SignedOut>
-    </div>
-    </div>
+          </SignedOut>
+        </div>
+      </div>
     </header>
     )
 }
