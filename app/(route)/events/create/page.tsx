644971847549071103dd5860718514f2da878522
ANASS RAISSI <88003906+anassraissi@users.noreply.github.com>
2024-03-13 00:20:58 +0000

    working on create Event => not finish

---
+++ app/(route)/events/create/page.tsx
@@ -0,0 +1,22 @@
+import EventForm from '@/components/shared/EventForm'
+import { auth } from '@clerk/nextjs'
+import React from 'react'
+const page = () => {
+    const {sessionClaims}=auth();   
+//const {sessionClaims}=auth();: Calls the auth function imported from @clerk/nextjs to get information about the current user's session. 
+//It destructures the sessionClaims object from the result.
+
+    const userId=sessionClaims?.userId as String;  // conserdret as string
+  return (
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+      <h3 className="wrapper h3-bold text-center sm:text-left">Create Event</h3>
+        </section>
+        <div className="wrapper my-8">
+            <EventForm userId={userId} type="create" />
+        </div>
+    </>
+  )
+}
+
+export default page
---
+++ components/shared/EventForm.tsx
@@ -0,0 +1,66 @@
+'use client'
+import React from 'react'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {Form,FormControl,FormDescription,FormField,FormItem,FormLabel,FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { EventFormSchema } from '@/lib/validator'
+import { z } from "zod"
+import { eventDefaultValues } from '@/app/constants'
+import Dropdown from './Dropdown'
+ 
+
+
+type EventFormProps={
+    userId : string,
+    type:'create | update'
+}
+const EventForm = ({userId,type}:EventFormProps) => {
+  const initialValues=eventDefaultValues;
+  const form = useForm<z.infer<typeof EventFormSchema>>({
+    resolver: zodResolver(EventFormSchema),
+    defaultValues: initialValues
+  })
+  function onSubmit(values: z.infer<typeof EventFormSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+  return (
+        <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5">
+        <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input placeholder="Event title" {...field} className="input-field" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+            <FormField
+            control={form.control}
+            name="categoryId"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Dropdown onChangeHandler={field.onChange} value={field.value} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+      </form>
+    </Form>
+    
+  )
+}
+
+export default EventForm
